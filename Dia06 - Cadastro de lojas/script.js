e4f56d3594f05854264ec041063cffc9967a055f
Marcus Vynicius <viniciusfla1999@gmail.com>
2024-10-15 22:15:11 +0100

    tirando a 'borda que destaca os inputs' (outline)

--- Dia06 - Cadastro de lojas/script.js
+++ Dia06 - Cadastro de lojas/script.js
@@ -1,16 +1,19 @@
 const btnName = document.getElementById('name');
 btnName.addEventListener('focus', (event) => {
-    event.target.style.border = '2px solid purple';
-}); // trocando a borda para roxo quando recebe o foco (focus)
+    event.target.style.border = '2px solid purple'; // trocando a borda para roxo quando recebe o foco (focus)
+    event.target.style.outline = 'none'; //tirando a 'borda destacada' (outline) que fica no input
+}); 
 
 btnName.addEventListener('blur', (event) => {
-    event.target.style.border = '';
-}); // tirando a borda quando o foco sair
+    event.target.style.border = ''; // tirando a borda quando o foco sair
+}); 
 
 const btnAdress = document.getElementById('adress');
 btnAdress.addEventListener('focus', (event) => {
-    event.target.style.border = '2px solid purple';
-}); // colocando a borda no adress com (focus)
+    event.target.style.border = '2px solid purple'; // colocando a borda no adress com (focus)
+
+    event.target.style.outline = 'none';
+}); 
 
 btnAdress.addEventListener('blur', (event) => {
     event.target.style.border = '';
@@ -18,8 +21,10 @@ btnAdress.addEventListener('blur', (event) => {
 
 const btnTimeOpen = document.getElementById('open');
 btnTimeOpen.addEventListener('focus', (event) => {
-    event.target.style.border = '2px solid purple';
-}); //focus
+    event.target.style.border = '2px solid purple'; //focus
+
+    event.target.style.outline = 'none';
+}); 
 
 btnTimeOpen.addEventListener('blur', (event) => {
     event.target.style.border = '';
@@ -27,8 +32,10 @@ btnTimeOpen.addEventListener('blur', (event) => {
 
 const btnTimeClosed = document.getElementById('closed');
 btnTimeClosed.addEventListener('focus', (event) => {
-    event.target.style.border = '2px solid purple';
-}); //focus
+    event.target.style.border = '2px solid purple'; //focus
+
+    event.target.style.outline = 'none';
+}); 
 
 btnTimeClosed.addEventListener('blur', (event) => {
     event.target.style.border = '';
